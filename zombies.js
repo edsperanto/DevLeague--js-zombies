cdef6a69a3c7d48e674e4a89e59f932690c994a0
Edward Gao <egao@sent.com>
2016-12-03 16:56:29 -1000

    created ExplodingZombie class

--- zombies.js
+++ zombies.js
@@ -416,7 +416,11 @@ class RangedZombie extends Zombie {
  * @param {number} strength         The zombie's strength.
  * @param {number} speed            The zombie's speed.
  */
-
+class ExplodingZombie extends Zombie {
+  constructor(health, strength, speed) {
+    super(health, strength, speed);
+  }
+}
 
 /**
  * ExplodingZombie Extends Zombie Class
